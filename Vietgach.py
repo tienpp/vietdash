7f45ce29149de1320ca8623adca63b980cf0bea8
unknown <heart2heart.it@gmail.com>
2015-05-14 00:44:55 +0700

    fix something

--- Vietgach.py
+++ Vietgach.py
@@ -5,20 +5,21 @@ word_dict = open('vv30K.index', 'r').readlines()
 def dash(source_text):
 	for line in word_dict:
 		word = line.split('\t')[0]
-		tmp_word = [' ' + word + ' ']
-		#Phần dưới tạo ra list các từ, để phòng trường hợp các từ ghép ngắn hơn được gạch ngang trước
-		#ví dụ A Di -> A Di thì sau đó A Di Đà không replace được nữa
-		for i in range(word.count(' ')):
-			tmp_word += [' ' + word.replace(' ', '-', i+1) + ' ']
-		for gen_word in tmp_word:
-			#đầu câu thì viết hoa
-			word_start = gen_word[1:].replace(gen_word[1:][0], gen_word[1:][0].upper(), 1)
-			if gen_word or word_start in source_text:
-				sdf = gen_word.replace(' ', '-')
-				source_text = source_text.replace(gen_word, sdf)
-
-				sdf = word_start.replace(' ', '-')
-				source_text = source_text.replace(word_start, sdf)
+		if ' ' in word:
+			tmp_word = [' ' + word + ' ']
+			#Phần dưới tạo ra list các từ, để phòng trường hợp các từ ghép ngắn hơn được gạch ngang trước
+			#ví dụ A Di -> A Di thì sau đó A Di Đà không replace được nữa
+			for i in range(word.count(' ')):
+				tmp_word += [' ' + word.replace(' ', '-', i+1) + ' ']
+			for gen_word in tmp_word:
+				#đầu câu thì viết hoa
+				word_start = gen_word[1:-1].replace(gen_word[1:-1][0], gen_word[1:-1][0].upper(), 1) + ' '
+				if gen_word in source_text:
+					sdf = ' ' + gen_word[1:-1].replace(' ', '-') + ' '
+					source_text = source_text.replace(gen_word, sdf)
+				elif  word_start in source_text:
+					sdf = word_start[:-1].replace(' ', '-') + ' '
+					source_text = source_text.replace(word_start, sdf)
 
 	return source_text
 
